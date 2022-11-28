930d3dd65f1196319859612f3686dc2853b081fb
Eljowe <98017040+Eljowe@users.noreply.github.com>
2022-11-28 08:56:34 +0200

    updated unicafe

--- Osa1/unikafe/app/src/App.js
+++ Osa1/unikafe/app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StatisticLine = (props) => {
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
 }
 
-export default App;
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value ={props.goodvalue} />
+        <StatisticLine text="neutral" value ={props.neutralvalue} />
+        <StatisticLine text="bad" value ={props.badvalue} />
+        <StatisticLine text="all" value ={props.goodvalue+props.neutralvalue+props.badvalue} />
+        <StatisticLine text="average" value ={(props.goodvalue+props.neutralvalue+props.badvalue)/3} />
+        <StatisticLine text="positive" value ={((props.goodvalue)/(props.goodvalue+props.neutralvalue+props.badvalue))*100} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  const [goodvalue, setgoodValue] = useState(0)
+  const [neutralvalue, setneutralValue] = useState(0)
+  const [badvalue, setbadValue] = useState(0)
+
+  if (goodvalue+neutralvalue+badvalue == 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setgoodValue(goodvalue+1)} text="good" />
+        <Button handleClick={() => setneutralValue(neutralvalue+1)} text="neutral" />
+        <Button handleClick={() => setbadValue(badvalue+1)} text="bad" />
+        <br/>
+        No feedback given
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setgoodValue(goodvalue+1)} text="good" />
+        <Button handleClick={() => setneutralValue(neutralvalue+1)} text="neutral" />
+        <Button handleClick={() => setbadValue(badvalue+1)} text="bad" />
+        <br/>
+        <Statistics goodvalue={goodvalue} badvalue={badvalue} neutralvalue={neutralvalue} />
+      </div>
+    )
+  }
+}
+
+export default App

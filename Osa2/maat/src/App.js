5fd0f9fe075dda8497e123cbca759ecda59600e8
Joel <xxstokexfanxx@gmail.com>
2022-12-01 21:16:21 +0200

    patches

--- Osa2/maat/src/App.js
+++ Osa2/maat/src/App.js
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import Countries from "./components/countriesController";
 
 function App() {
   const [newSearch, setNewSearch] = useState("");
   const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
   const handleSearchChange = event => {
     setNewSearch(event.target.value);
   };
 
-  
+  const countriesToShow = newSearch.length < 1
+    ? []
+    : countries.filter(country => country.name.common.toUpperCase().includes(newSearch.toUpperCase()))
+      
+
+
+  return(
+    <div>
+      find countries
+      <input value={newSearch} onChange={handleSearchChange}/>
+      <Countries countriesToShow={countriesToShow}/>
+    </div>
+  )
+
 }
 
 export default App;
---
+++ Osa2/maat/src/components/countriesController.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Countries = (props) => {
+    console.log(props.countriesToShow, props.countriesToShow.length)
+    if (1<props.countriesToShow.length && props.countriesToShow.length<11) { 
+        return(
+            <div>
+                <ul>
+                    {props.countriesToShow.map(country =>
+                        <li key={country.name.common}>{country.name.common}</li>)}
+                </ul>
+            </div>
+        )
+    } else if (props.countriesToShow.length>11) {
+        return(
+            <div>
+                Too many matches, specify another filter
+            </div>
+        )
+    } else if (props.countriesToShow.length == 1) {
+        
+        <div>
+            <h1>{props.countriesToShow}</h1>
+        </div>
+    }
+}
+
+export default Countries

d5c43607495218d82ea0c11b60d12df0c73026ba
Joel <xxstokexfanxx@gmail.com>
2022-12-08 18:49:16 +0200

    ok

--- Osa3/3/index.js
+++ Osa3/3/index.js
@@ -2,11 +2,39 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
+let persons = [
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      {
+        "name": "b",
+        "number": "2",
+        "id": 6
+      }
+  ]
 
+
+//'http://localhost:3001/persons'
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
 app.get('/persons', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.json(persons)
 })
 
 const PORT = process.env.PORT || 3002;

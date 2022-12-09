598b796bca9461e42953493a6e192ba0a3940028
Joel <xxstokexfanxx@gmail.com>
2022-12-09 13:44:49 +0200

    idk

--- Osa2/puhelinluettelo/src/services/DB.js
+++ Osa2/puhelinluettelo/src/services/DB.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 const listPersons = () => {
-    const res = axios.get('http://localhost:3001/persons');
+    const res = axios.get('http://localhost:8080/api/persons');
     return res.then(response => response.data);
 };
 
 const add = obj => {
-    const res = axios.post('http://localhost:3001/persons', obj);
+    const res = axios.post('http://localhost:8080/api/persons', obj);
     return res.then(response => response.data);
 };
 
 const update = (id, obj) => {
-    const res = axios.put(`http://localhost:3001/persons/${id}`, obj);
+    const res = axios.put(`http://localhost:8080/api/persons/${id}`, obj);
     return res.then(response => response.data);
 };
 
 const deletePerson = id => {
-    const res = axios.delete(`http://localhost:3001/persons/${id}`);
+    const res = axios.delete(`http://localhost:8080/api/persons/${id}`);
     return res.then(response => response.data);
   };
 
--- Osa3/3/index.js
+++ Osa3/3/index.js
@@ -1,43 +1,115 @@
+const cors = require('cors')
 const http = require('http')
 const express = require('express')
 const app = express()
+var morgan = require("morgan");
+
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+app.use(morgan("tiny"));
+app.use(express.json())
+app.use(cors())
+
+morgan.token("Body", req => JSON.stringify(req.body));
 
 let persons = [
+{
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+    },
     {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "b",
-        "number": "2",
-        "id": 6
-      }
-  ]
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+    },
+    {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+    },
+    {
+    "name": "b",
+    "number": "2",
+    "id": 6
+    }
+]
+
+const generateId = () => {
+const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+return maxId + 1
+}
+
 
 
 //'http://localhost:3001/persons'
-app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello')
 })
-
-app.get('/persons', (req, res) => {
+//api/persons
+app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-const PORT = process.env.PORT || 3002;
+app.get('/api/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people \n ${Date.now()}`)
+})
+
+app.get('/api/persons/:id', (request, res) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (request, res) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'name or number missing' 
+        })
+    }
+
+    if (persons.filter(person => person.name===body.name).length > 0) {
+    return response.status(400).json({ 
+        error: 'name must be unique' 
+        })
+    }
+
+    const person = {
+        name: body.name,
+        important: body.number,
+        id: generateId(),
+      }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

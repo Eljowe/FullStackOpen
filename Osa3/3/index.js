c5ebefb7d2e11620017f9e6ec310c5b501b9be51
Eljowe <98017040+Eljowe@users.noreply.github.com>
2022-12-15 15:43:29 +0200

    middleware

--- Osa3/3/index.js
+++ Osa3/3/index.js
@@ -14,11 +14,12 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+app.use(express.static('build'))
+app.use(express.json())
 app.use(requestLogger)
 app.use(morgan("tiny"));
-app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
+
 
 morgan.token("Body", req => JSON.stringify(req.body));
 const { response } = require('express');
@@ -33,24 +34,43 @@ app.get('/', (req, res) => {
 //api/persons
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-        response.json(persons)
-    })
+        if (persons) {
+            response.json(persons)
+        } else {
+            response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
 })
 
 app.get('/api/info', (req, res) => {
-    res.send(`Phonebook has info for ${persons.length} people \n ${Date.now()}`)
+    Person.count({}, function( err, count){
+        res.send(`Phonebook has info for ${count} people \n ${Date.now()}`)
+    })
+    
 })
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
       })
+      .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    const person_id = Number(request.params.id)
+    Person.deleteOne({id: person_id}).then(persons => {
+        if (persons) {
+            response.status(204).end()
+        } else {
+            response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response) => {
@@ -68,12 +88,39 @@ app.post('/api/persons/', (request, response) => {
       })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
   
 app.use(unknownEndpoint)
 
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+  // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+  app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

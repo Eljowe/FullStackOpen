60a3ffa1b05e6114c1b866d94b1b829714e2b454
Joel <xxstokexfanxx@gmail.com>
2023-01-09 15:59:56 +0200

    api tests

---
+++ Osa4/blog/tests/blog_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const testblogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+]
+
+describe('Test backend', () => {
+    test('blogs are returned as json', async () => {
+        await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('blogs have id in correct format', async () => {
+        const res = await api.get('/api/blogs')
+        res.body.forEach(element => {
+            expect(element.id).toBeDefined()
+        });
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})

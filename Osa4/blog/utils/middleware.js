017f4617e1156365a1963c65f74f6ad9fe531594
Joel <xxstokexfanxx@gmail.com>
2023-01-10 16:52:33 +0200

    middleware imports fix

--- Osa4/blog/utils/middleware.js
+++ Osa4/blog/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)

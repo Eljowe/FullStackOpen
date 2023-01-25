4b7ca458d5ba8155b466fd5d7c1eac26cdf0b10e
Eljowe <98017040+Eljowe@users.noreply.github.com>
2023-01-25 11:13:00 +0200

    6.9

---
+++ Osa6/redux-anecdotes/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+
+const store = configureStore({
+    reducer: {
+        anecdotes: anecdoteReducer,
+    }
+})
+
+export default store

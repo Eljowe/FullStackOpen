365569ae8deb9965d7a81ab4c8bc4dd08f4a7cdd
Joel <xxstokexfanxx@gmail.com>
2023-01-26 18:50:18 +0200

    minor refactoring of app

--- Osa6/redux-anecdotes/src/App.js
+++ Osa6/redux-anecdotes/src/App.js
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Filter />
       <Notification/>
+      <Filter />
       <AnecdoteList/>
       <NewAnecdote/>
     </div>
--- Osa6/redux-anecdotes/src/components/Filter.js
+++ Osa6/redux-anecdotes/src/components/Filter.js
@@ -2,6 +2,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
 
 const Filter = () => {
+    const style = {
+        marginBottom: 10
+      }
     const dispatch = useDispatch()
     const filterfunc = (event) => {
         
@@ -13,9 +16,8 @@ const Filter = () => {
         }
     }
     return(
-        <div>
-            Filter
-            <form onChange={filterfunc}>
+        <div style={style}>
+            Filter <form onChange={filterfunc}>
                 <input name="filter"/>
             </form>
         </div>

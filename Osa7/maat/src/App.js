d4988d8e79687d6887cd9d95667d11bce40c5298
Joel <xxstokexfanxx@gmail.com>
2023-02-06 13:34:35 +0200

    finished course, leaving part 7 unfinished

--- Osa7/maat/src/App.js
+++ Osa7/maat/src/App.js
@@ -34,9 +34,10 @@ const ShowCountry = ({ country }) => {
 	return (
 		<div>
 			<h3>{country.name.common}</h3>
-			<div>capital {country.capital}</div>
-			<div>population {country.population}</div>
-			<img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`} />
+			<div>capital: {country.capital}</div>
+			<div>population: {country.population}</div>
+
+			<img src={country.flags.png} height='230' alt={`${country.name.common} flag` } />
 		</div>
 	)
 }
